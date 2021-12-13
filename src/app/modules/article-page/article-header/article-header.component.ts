7cf9edf1dd9e32060cae4330356cb1f137293454
caonam.uit@gmail.com <caonam.uit@gmail.com>
2021-12-13 15:20:10 +0700

    update

--- src/app/modules/article-page/article-header/article-header.component.ts
+++ src/app/modules/article-page/article-header/article-header.component.ts
@@ -22,6 +22,7 @@ export class ArticleHeaderComponent implements OnInit {
   }
 
   OnNavigate() {
+    // ggfgfg
     let slug = this.articleData ? this.articleData.currentArticle.slug : '';
     this.router.navigate(['/editor', slug]);
   }
